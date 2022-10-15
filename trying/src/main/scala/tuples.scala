aefcae9b45bd62db3a5dfb57c913b927a4eb5d51
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-10-15 18:06:24 +0530

    tuples added

---
+++ trying/src/main/scala/tuples.scala
@@ -0,0 +1,21 @@
+object tuples {
+  val mytuple = (1,2,"hello",true,(4,5));
+  val mytuple2 = new Tuple3(1,2,"hello")
+  /* This is wrong now
+  val mytuple3 = new Tuple3(1,2)
+  becaues Tuple3 means tuple me 3 elements
+  this goes till Tuple22
+   */
+  def main(args: Array[String]): Unit ={
+    println(mytuple)
+    println(mytuple._2)
+    println(mytuple._4)
+    println(mytuple._3)
+    mytuple.productIterator.foreach{
+      i => println(i)
+    }
+    println(1 -> "Tom" -> true)
+    println(mytuple._5._2);
+  }
+
+}
