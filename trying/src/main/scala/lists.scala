5952c0f405fb8ba055f2aaca187a3b6f11dcd11e
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-10-15 12:11:47 +0530

    lists added

---
+++ trying/src/main/scala/lists.scala
@@ -0,0 +1,26 @@
+import Array._
+/* Arrays are mutable and
+lists are immutable, works as linkedlist
+ arrays are flat*/
+object lists {
+
+  val mylist: List[Int] = List(1,2,3,4,5,6)
+  val names: List[String] = List("Max","Tom","John")
+  def main(args: Array[String]): Unit ={
+    println(mylist)
+    println(names)
+
+    /*prepend hojaega 0 BUT MYLIST CHANGE NHI HOGI*/
+    println(0 :: mylist)
+    println(1 :: 5 :: 9 :: Nil)
+    println(mylist.head)
+    /* tail is everything except header */
+    println(names.tail)
+    println(names.isEmpty)
+    
+    println(List.fill(5)(2))
+
+
+  }
+
+}
